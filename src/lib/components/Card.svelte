82725a329d5be42dec908b37036a4b434e96b56a
hozunlee <ho2yahh@gmail.com>
2023-08-22 01:32:59 +0900

    feat : 메인 페이지 작업

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,76 @@
+<script>
+	export let card
+	import dayjs from 'dayjs'
+
+	const publishedAt = dayjs(card.attributes.publishedAt).format('YY/MM/DD')
+	const keywords = card.attributes.keyword.replace(/(\s*)/g, '').split(',')
+	const shortDesc = card.attributes.desc.slice(0, 100)
+
+	const imgSrc = card.attributes.cover.data.attributes.url
+</script>
+
+<article class="">
+	<header>
+		<a href={`/post/${card.id}`}>
+			<div class="post-thumbnail">
+				<!-- <NextImage image={card.attributes.main_cover} /> -->
+				<img src={imgSrc} alt="커버이미지" />
+			</div>
+		</a>
+	</header>
+	<h2 class="card-title">{card.attributes.title}</h2>
+	<p>
+		{shortDesc}... <a href={`/post/${card.id}`}>더보기</a>
+	</p>
+	<div class="card-time">{publishedAt}</div>
+	<footer>
+		{#each keywords as word}
+			<!-- content here -->
+			<span class="card-keyword">
+				{word}
+			</span>
+		{/each}
+	</footer>
+</article>
+
+<style>
+	article {
+		margin: 1rem;
+	}
+	header {
+		box-sizing: border-box;
+		max-width: 700px;
+		margin: 0 auto;
+		margin-bottom: calc(1rem * 2);
+	}
+	.post-thumbnail {
+		width: 100%;
+		position: relative;
+		overflow: hidden;
+		padding-top: 50%; /* 직사각형 비율에 따라 변경하세요 (예: 가로 2: 세로 1 = 50%) */
+		padding-left: 2rem;
+		padding-right: 2rem;
+		/* max-height: 386px;
+		width: 100%;
+		position: relative;
+		box-sizing: border-box;
+		overflow: hidden;
+		display: flex;
+		align-items: center;
+		justify-content: center; */
+	}
+
+	.post-thumbnail > img {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		object-fit: cover;
+		/* border: none;
+		padding: 1rem;
+		object-fit: cover;
+		height: 100%;
+		inset: 0px; */
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <script>
 	import Cake from '$lib/components/Cake.svelte'
+	import Card from '$lib/components/Card.svelte'
 	import { onMount } from 'svelte'
 
 	export let data
 	let arrayLength = data.cakes.length
 
-	$ : console.log('data.posts :>> ', data.posts);
-
+	$: console.log('data.posts :>> ', data.posts)
 
 	/**
-	 * 
+	 *
 	 * @param arrayLength {number}
 	 */
 	const 랜덤숫자 = (arrayLength) => {
-		return Math.floor(Math.random() * arrayLength);
+		return Math.floor(Math.random() * arrayLength)
 	}
 
 	const 메인문장만들기 = () => {
@@ -29,15 +29,15 @@
 	 * @property {string} [speaker] 책 안에서 말한 사람이 다를 경우
 	 */
 
-   	/** @type {cakeItem} */
+	/** @type {cakeItem} */
 	let 메인문장
-	
+
 	onMount(() => {
 		// content here
 		메인문장 = data.cakes[랜덤숫자(data.cakes.length)]
-	});
-	
-	// 
+	})
+
+	//
 </script>
 
 <svelte:head>
@@ -45,18 +45,16 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-
-	{#if 메인문장}
-		 <div class="메인문장박스">
-			<button on:click={메인문장만들기} >new 🧀</button>
-			<Cake item={메인문장} />
-			<!-- <div class="button">
+{#if 메인문장}
+	<div class="메인문장박스">
+		<button on:click={메인문장만들기}>new 🧀</button>
+		<Cake item={메인문장} />
+		<!-- <div class="button">
 				<Button>new</Button>
 			</div> -->
-		</div>
-	{/if}
-
-
+	</div>
+{/if}
+<div class="구분선 div-transparent" />
 <section>
 	<h1>
 		<!-- <span class="welcome">
@@ -66,13 +64,11 @@
 			</picture>
 		</span> -->
 
-		Dev Hololog
+		{#each data.posts as post}
+			<!-- content here -->
+			<Card card={post} />
+		{/each}
 	</h1>
-
-
-
-	<div>프리텐다드</div>
-	적용된삘?
 </section>
 
 <style>
@@ -91,24 +87,40 @@
 	.메인문장박스 {
 		margin-top: 5rem;
 		text-align: center;
-	line-height: 1.3;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-
+		line-height: 1.3;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
 	}
 
 	.메인문장박스 > button {
 		border: none;
-color: gray;
-font-size: small;
-cursor: pointer;
+		color: gray;
+		font-size: small;
+		cursor: pointer;
 	}
 
-/* .button {
+	/* .button {
 		display: inline-block;
 		width: 300px;
 		margin: 40px 0 50px 0;
 	} */
+
+	.구분선 {
+		position: relative;
+		margin-top: 10px;
+		height: 1px;
+	}
+
+	.div-transparent:before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 5%;
+		right: 5%;
+		width: 90%;
+		height: 1px;
+		background-image: linear-gradient(to right, transparent, rgb(48, 49, 51), transparent);
+	}
 </style>
