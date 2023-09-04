1d1e4f09c39c85b49997f526c4414e4c7c7e6d79
hozunlee <ho2yahh@gmail.com>
2023-09-04 10:57:40 +0900

    deploy : isr 적용

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +7,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'edge',
+			isr: {
+				expiration: 60
+			}
+		})
 	}
-};
+}
 
-export default config;
+export default config
