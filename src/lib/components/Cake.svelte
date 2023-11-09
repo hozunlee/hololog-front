9031c085c4ec8a8c4a7d91b69291f3c483cc86ea
hozunlee <ho2yahh@gmail.com>
2023-11-09 17:16:02 +0900

    style : cake p  padding adjust

--- src/lib/components/Cake.svelte
+++ src/lib/components/Cake.svelte
@@ -1,5 +1,4 @@
 <script>
-
 	/**
 	 *
 	 * @typedef {Object} cakeItem
@@ -10,48 +9,45 @@
 	 * @property {string} [speaker] 책 안에서 말한 사람이 다를 경우
 	 */
 
-   	/** @type {cakeItem} */
-export let item
+	/** @type {cakeItem} */
+	export let item
 </script>
 
 <section>
+	<div class="문장-박스">
+		<p class="문장">{item.item}</p>
 
-<div class="문장-박스">
-<p class="문장">{item.item}</p>
-
-<div class="출처-박스">
-<p>from</p>
-<p>{item.from}</p>
-<p>{item.speaker ?? item.author}</p>
-</div>
-</div>
+		<div class="출처-박스">
+			<p>{item.speaker ?? item.author}</p>
+			<p>from</p>
+			<p>{item.from}</p>
+		</div>
+	</div>
 </section>
 
-
 <style>
+	section {
+		text-align: center;
+		line-height: 1.3;
+		max-width: 54rem;
+	}
 
-section {
+	.문장-박스 {
+		padding: 0.5rem;
+	}
 
-  text-align: center;
-	line-height: 1.3;
-	max-width: 54rem;
+	.문장 {
+		padding-bottom: 1em;
+		font-size: 0.95rem;
+		letter-spacing: -0.3px;
+	}
 
-}
+	.출처-박스 {
+		font-size: 0.7rem;
+		color: #7b7b7b;
+	}
 
-.문장-박스 {
-  padding: 1rem;
-}
-
-.문장 {
-  padding-bottom: 1em;
-  font-size: 0.95rem;
-  letter-spacing: -0.3px;
-}
-
-.출처-박스 {
-  font-size: 0.7rem;
-  color: #7b7b7b;
-
-}
-
+	.출처-박스 > p {
+		padding: 0;
+	}
 </style>
