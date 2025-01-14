2825fbe374263b87d370110419a316d4c3abb446
hozunlee <ho2yahh@gmail.com>
2025-01-14 11:48:55 +0900

    feat : 문장 업데이트 추가

---
+++ src/routes/cake/add/+page.server.js
@@ -0,0 +1,71 @@
+import { supabase } from '$lib/supabase/supabaseClient'
+import { fail, redirect } from '@sveltejs/kit'
+
+export async function load({ url }) {
+	const q = url.searchParams.get('q')
+
+	// cake page
+	const page = parseInt(url.searchParams.get('page') || '1')
+	const pageSize = 10
+	const start = (page - 1) * pageSize
+	const end = start + pageSize - 1
+
+	if (!q || q !== 'admin') {
+		throw redirect(303, '/') // throw를 추가하여 리디렉션 실행
+	}
+
+	const { data, error, count } = await supabase
+		.from('cakes')
+		.select('id, author, from, item, speaker', { count: 'exact' })
+		.order('id', { ascending: false })
+		.range(start, end)
+
+	// console.log('data :>> ', data)
+
+	return {
+		items: data,
+		totalPages: Math.ceil(count / pageSize),
+		currentPage: page
+	}
+}
+
+export const actions = {
+	addCake: async ({ request }) => {
+		const formData = await request.formData()
+
+		const item = formData.get('item')
+		const from = formData.get('from')
+		const author = formData.get('author')
+		const speaker = formData.get('speaker')
+
+		if (!item || !from || !author) {
+			return fail(400, {
+				error: '모든 필드를 입력해주세요'
+			})
+		}
+
+		console.log('addCake')
+		const { data, error } = await supabase
+			.from('cakes')
+			.insert([
+				{
+					item,
+					from,
+					author,
+					speaker
+				}
+			])
+			.select()
+		console.log(error, '에러입니다아앙아아')
+
+		console.log('인서트 박았니?????????????????/')
+		console.log('성공 data :>> ', data)
+		if (error) {
+			return fail(500, {
+				error: '저장 중 오류가 발생했습니다'
+			})
+		}
+
+		return { success: true }
+	}
+}
---
+++ src/routes/cake/add/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { Input } from '$lib/components/ui/input/index.js'
+	import { Label } from '$lib/components/ui/label/index.js'
+	import { Textarea } from '$lib/components/ui/textarea/index.js'
+	import { Button } from '$lib/components/ui/button/index.js'
+	export let data
+
+	console.log('data :>> ', data)
+</script>
+
+<section class="container my-10 mx-5">
+	<h1>케이크 추가</h1>
+	<h2>새 문장 추가</h2>
+	<form action="?/addCake" method="post" class="p-1 gap-3">
+		<div class="grid w-full gap-1.5">
+			<Label for="item">문장</Label>
+			<Textarea name="item" placeholder="나를 생각하게 했던 문장을 적어라" id="item" />
+		</div>
+		<div>
+			<Label for="from">출처</Label>
+			<Input name="from" id="from" />
+		</div>
+		<div>
+			<Label for="author">저자</Label>
+			<Input name="author" id="author" />
+		</div>
+		<div>
+			<Label for="speaker">화자</Label>
+			<Input name="speaker" id="speaker" />
+		</div>
+		<Button type="submit" class="my-5">보내기</Button>
+	</form>
+
+	<h2>문장 목록</h2>
+</section>
